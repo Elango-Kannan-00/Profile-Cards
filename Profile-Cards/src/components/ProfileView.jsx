90d440574208b1e96f3f65e1da877049043e46c4
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-11-02 01:07:43 +0530

    feat: refactor profile-cards

--- Profile-Cards/src/components/ProfileView.jsx
+++ Profile-Cards/src/components/ProfileView.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const ProfileView = ({emp, onClose}) => {
+const ProfileView = ({ emp, onClose }) => {
   if (!emp) return null;
   return (
-    <div className='profileview-container'>
-        <img src={emp.image} alt="Profile-Image" />
-        <h2>{emp.name}</h2>
-        <p><strong>Role:</strong> {emp.role}</p>
-        <p><strong>Email:</strong> {emp.email}</p>
-        <p><strong>Location:</strong> {emp.location}</p>
-        <p><strong>Skills:</strong> {emp.skills.join(", ")}</p>
-        <p><strong>Bio:</strong> {emp.bio}</p>
-        <button onClick={onClose}>Close Profile</button>
+    <div className="profileview-container">
+      <img src={emp.image} alt="Profile-Image" />
+      <h2>{emp.name}</h2>
+      <p>
+        <strong>Role:</strong> {emp.role}
+      </p>
+      <p>
+        <strong>Email:</strong> {emp.email}
+      </p>
+      <p>
+        <strong>Location:</strong> {emp.location}
+      </p>
+      <p>
+        <strong>Skills:</strong> {emp.skills.join(", ")}
+      </p>
+      <p>
+        <strong>Bio:</strong> {emp.bio}
+      </p>
+      <button onClick={onClose}>Close Profile</button>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileView
+export default ProfileView;
