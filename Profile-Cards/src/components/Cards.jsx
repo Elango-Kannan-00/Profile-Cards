4fdc6266267bffb699eec29e3bdb2c76d554a011
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-11-02 01:07:39 +0530

    feat: refactor profile-cards

--- Profile-Cards/src/components/Cards.jsx
+++ Profile-Cards/src/components/Cards.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import '../App.css';
+import "../App.css";
 
-const Cards = ({emp, viewProfile}) => {
+const Cards = ({ emp, viewProfile }) => {
   return (
-    <div className='card-container'>
-        <div className='photo-section'>
-            <img src={emp.image} alt="Profile-Photo" />
-        </div>
-        <div className='content-section'>
-            <h4>{emp.name}</h4>
-            <h5><i>{emp.role}</i></h5>
-            <button onClick={viewProfile}>View Profile</button>
-        </div>
+    <div className="card-container">
+      <div className="photo-section">
+        <img src={emp.image} alt="Profile-Photo" />
+      </div>
+      <div className="content-section">
+        <h4>{emp.name}</h4>
+        <h5>
+          <i>{emp.role}</i>
+        </h5>
+        <button onClick={viewProfile}>View Profile</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
